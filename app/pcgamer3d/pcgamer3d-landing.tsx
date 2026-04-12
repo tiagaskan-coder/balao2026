782f8cdb5fc134fcfa83984a57f6f7e603e92d19
balao2026 <dev@balao.info>
2026-04-12 19:55:37 -0300

    chore: increase 3d viewer scale

--- app/pcgamer3d/pcgamer3d-landing.tsx
+++ app/pcgamer3d/pcgamer3d-landing.tsx
@@ -227,10 +227,10 @@ function Hero() {
                   src={`https://sketchfab.com/models/${MODEL_ID}/embed?ui_theme=dark&transparent=1&autostart=1&ui_infos=0&ui_watermark=0&ui_controls=0&ui_general_controls=0&ui_fullscreen=0&ui_help=0&ui_hint=0&ui_vr=0&ui_settings=0&ui_annotations=0&ui_stop=0&camera=0&dnt=1`}
                   className="absolute bg-transparent"
                   style={{
-                    top: -130,
-                    left: -40,
-                    width: "calc(100% + 80px)",
-                    height: "calc(100% + 260px)",
+                    top: -160,
+                    left: -55,
+                    width: "calc(100% + 110px)",
+                    height: "calc(100% + 320px)",
                   }}
                   allow="autoplay; fullscreen; xr-spatial-tracking"
                   allowFullScreen
